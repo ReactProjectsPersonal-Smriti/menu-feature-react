7e8443117f25a46df3c2aaa9a64a4efc9431af5c
Smriti7111 <smritibanjade71@gmail.com>
2021-04-27 11:12:51 +0545

    add menu feature

---
+++ src/Categories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Categories = ({ filterItems, categories }) => {
+  return (
+    <div className="btn-container">
+      {console.log(categories)}
+      {categories.map((category, index) => {
+        return (
+          <button key={index} className='filter-btn' onClick={() => { filterItems(category) }}>{category}</button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
